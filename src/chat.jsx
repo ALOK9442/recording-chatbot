ae2cca32398a539c0e5e3e245b8c59dc2b5215b5
ALOK9442 <alok2110036@akgec.ac.in>
2023-12-27 17:04:03 +0530

    feat: added user and sender interface in chat

--- src/chat.jsx
+++ src/chat.jsx
@@ -7,6 +7,7 @@ const ChatPage = () => {
     const [chatMessages, setChatMessages] = useState([]);
     const [isRecording, setIsRecording] = useState(false);
     const [audioData, setAudioData] = useState([]);
+    const [currentSender, setCurrentSender] = useState('chat');
     const audioRef = useRef(null);
     const mediaRecorderRef = useRef(null);
 
@@ -47,11 +48,17 @@ const ChatPage = () => {
     const toggleRecording = () => {
         if (!isRecording) {
             startRecording();
+            setCurrentSender('user');
         } else {
             stopRecording();
+            setCurrentSender('chat');
         }
     };
 
+    const handleMessage = () => {
+        setChatMessages((prevChatMessages) => [...prevChatMessages, { text: inputText, isUser: true }]);
+        setInputText('');
+    }
 
     useEffect(() => {
         if (audioData.length > 0) {
@@ -66,29 +73,37 @@ const ChatPage = () => {
         <div className="container mx-auto mt-8 p-4 flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-4">Voice Note Chatbot</h1>
             <div className="h-96 overflow-y-scroll border border-gray-300 p-4 w-80">
-                <div>
-                    {audioData.map((audio, index) => (
-                        <div key={index} className={`mb-2 ${audio.isUser ? 'text-right' : 'text-left'}`}>
-                            <audio controls src={audio.url} className='w-40' />
-                        </div>
-                    ))}
-                </div>
-                <div className="flex items-center mt-48">
-                    <input
-                        type="text"
-                        value={inputText}
-                        onChange={(e) => setInputText(e.target.value)}
-                        placeholder="Type your message..."
-                        className="flex-1 border p-2 rounded-l-md"
-                    />
-                    <div className=''>
-                        <button className={`bg-amber-500 p-2 rounded-md ${isRecording ? 'bg-red-500' : ''}`} onClick={toggleRecording}>
-                            {isRecording ? <FontAwesomeIcon icon={faStop}/> : <FontAwesomeIcon icon={faMicrophone}/>}
-                        </button>
-                        <button className="bg-blue-500 text-white p-2 rounded-r-md">
-                            <FontAwesomeIcon icon={faPaperPlane}/>
-                        </button>
+                {chatMessages.map((message, index) => (
+                    <div key={index} className={`mb-2 ${message.isUser ? 'text-right' : 'text-left'}`}>
+                        <p>{message.text}</p>
                     </div>
+                ))}
+                {audioData.map((audio, index) => (
+                    <div key={index} className={`ml-36 ${audio.isUser ? 'text-right' : 'text-left'}`}>
+                        <audio controls src={audio.url} className='w-40' />
+                    </div>
+                ))}
+                {audioData.map((audio, index) => (
+                    <div key={index} className={`mb-2 ${audio.isUser ? 'text-right' : 'text-left'}`}>
+                        <audio controls src={audio.url} className='w-40' />
+                    </div>
+                ))}
+            </div>
+            <div className="flex items-center space-x-6 w-80">
+                <input
+                    type="text"
+                    value={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
+                    placeholder="Type your message..."
+                    className="flex-1 border p-2 rounded-l-md"
+                />
+                <div className='space-x-6'>
+                    <button className={`bg-amber-500 p-2 rounded-md ${isRecording ? 'bg-red-500' : ''}`} onClick={toggleRecording}>
+                        {isRecording ? <FontAwesomeIcon icon={faStop} /> : <FontAwesomeIcon icon={faMicrophone} />}
+                    </button>
+                    <button className="bg-blue-500 text-white p-2 rounded-md">
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
                 </div>
             </div>
         </div>
